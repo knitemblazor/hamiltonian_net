bf58d428023e848cf59a48d73e1272798222feda
NITISH TOM MICHAEL <niteshmichael@gmail.com>
2020-02-20 23:22:00 +0530

    Update neural_net.py

--- neural_net.py
+++ neural_net.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from config import space_points_gen
 
 
 class Net(nn.Module):
@@ -7,15 +8,20 @@ class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
         self.fc1 = nn.Linear(1, 8, bias=False)
-        self.fc2 = nn.Linear(8, 1)
+        self.fc2 = nn.Linear(8, 1, bias=False)
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = torch.sigmoid(self.fc2(x))
+        x = torch.sigmoid(self.fc1(x))
+        x = self.fc2(x)
         return x
 
 
+xpointe = space_points_gen()
 net = Net()
+print(list(net.parameters()))
+input = torch.tensor([[8.5]])
+out = net(input)
+
 print(list(net.parameters()))
 input = torch.tensor([[8.5]])
 out = net(input)
