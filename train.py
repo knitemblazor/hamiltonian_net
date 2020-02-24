1192563d12ab22a73be725954250eb06d3fa8330
NITISH TOM MICHAEL <niteshmichael@gmail.com>
2020-02-24 18:39:49 +0530

    Update train.py

--- train.py
+++ train.py
@@ -4,11 +4,11 @@ import torch.optim as optim
 
 net = Net()
 x_state = space_points_gen()
-optimizer = optim.SGD(net.parameters(), lr=0.08)
+optimizer = optim.Adam(net.parameters(), lr=0.08)
 
-for i in range(40):
+for i in range(200):
     optimizer.zero_grad()
     loss = error_function(beta, net)
-    print(loss)
+    print("loss:", loss.detach().numpy()[0][0])
     loss.backward()
     optimizer.step()
