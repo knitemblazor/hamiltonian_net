d846709e4b93751be31e6d223aaa926fbf8fa56d
NITISH TOM MICHAEL <niteshmichael@gmail.com>
2020-02-24 14:38:27 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,25 +1,14 @@
-# not complete need to write the loss function
-from hamiltonian_net.methods import *
-from hamiltonian_net.neural_net import *
+from methods import *
+from neural_net import *
 import torch.optim as optim
 
 net = Net()
 x_state = space_points_gen()
-optimizer = optim.SGD(net.parameters(), lr=0.1)
+optimizer = optim.SGD(net.parameters(), lr=0.08)
 
-for i in range(400):
-    for x in x_state:
-        inter = []
-        inter.append(x)
-        intera = []
-        intera.append(inter)
-        x = torch.as_tensor(intera).unsqueeze(0)
-        optimizer.zero_grad()
-        out = net(x)
-        loss = error_function(beta, out)
-        print(loss)
-        loss.backward()
-        optimizer.step()
-
-
-
+for i in range(40):
+    optimizer.zero_grad()
+    loss = error_function(beta, net)
+    print(loss)
+    loss.backward()
+    optimizer.step()
