72d98a0ce2a13027a902f926770181b8f044053c
NITISH TOM MICHAEL <niteshmichael@gmail.com>
2020-03-14 17:39:23 +0530

    Update neural_net.py

--- neural_net.py
+++ neural_net.py
@@ -6,8 +6,8 @@ class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(1, 8, bias=False)
-        self.fc2 = nn.Linear(8, 1, bias=False)
+        self.fc1 = nn.Linear(1, 8)
+        self.fc2 = nn.Linear(8, 1)
 
     def forward(self, x):
         x = torch.sigmoid(self.fc1(x))
